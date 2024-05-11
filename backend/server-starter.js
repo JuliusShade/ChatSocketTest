99c9740c25eafb02051f0c2357b268a9e42c432c
JuliusShade <shadejr.business@gmail.com>
2024-05-11 02:37:31 -0400

    Added the socket functionality and server boilerplate to the server-starter.js file

--- backend/server-starter.js
+++ backend/server-starter.js
@@ -3,29 +3,29 @@ import { uniqueNamesGenerator, colors, names } from "unique-names-generator";
 import express from "express";
 import http from "http";
 
+// Step 1
 // import the socket.io library
 import { Server } from "socket.io";
 
-// initializing the servers: HTTP as well as Web Socket
+// Step 2
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-// create the chat history array for storing messages
+// Step 3
 const chatHistory = [];
 
-// listen for new web socket connections
+// Step 4
 io.on("connection", function callback(socket) {
   const username = getUniqueUsername();
   console.log(`${username} connected`);
 
-  // send the chat history to the client
   socket.emit("receive-messages", {
     chatHistory: getAllMessages(),
     username,
   });
 
-  // listen for new messages from the client
+  // Step 5
   socket.on("post-message", function receiveMessages(data) {
     const { message } = data || { message: "" };
     console.log(message);
@@ -34,38 +34,17 @@ io.on("connection", function callback(socket) {
       message,
     });
 
-    // send the updated chat history to all clients
     io.emit("receive-messages", {
       chatHistory: getAllMessages(),
     });
   });
 
-  // listen for disconnects and log them
   socket.on("disconnect", () => {
     console.log(`${username} disconnected`);
   });
 });
 
-// Boilerplate code as well as Bonus section
-// HTTP server setup to serve the page assets
-app.use(express.static(process.cwd() + "/frontend"));
-
-// HTTP server setup to serve the page at / route
-app.get("/", (req, res) => {
-  return res.sendFile(process.cwd() + "/frontend/index.html");
-});
-
-// start the HTTP server to serve the page
-server.listen(3000, () => {
-  console.log("listening on http://localhost:3000");
-});
-
-// helper functions
-// get all messages in the order they were sent
-function getAllMessages() {
-  return Array.from(chatHistory).reverse();
-}
-
+// HELPER FUNCTIONS
 // generate a unique username for each user
 function getUniqueUsername() {
   return uniqueNamesGenerator({
@@ -75,3 +54,21 @@ function getUniqueUsername() {
     separator: " ",
   });
 }
+
+function getAllMessages() {
+  return Array.from(chatHistory).reverse();
+}
+
+// SERVER BOILERPLATE
+// HTTP server setup to serve the page assets
+app.use(express.static(process.cwd() + "/frontend"));
+
+// HTTP server setup to serve the page at /
+app.get("/", (req, res) => {
+  return res.sendFile(process.cwd() + "/frontend/index.html");
+});
+
+// start the HTTP server to serve the page
+server.listen(3000, () => {
+  console.log("listening on http://localhost:3000");
+});
